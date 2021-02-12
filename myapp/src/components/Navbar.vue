c38fe8d91a3f2f143ee3e14f5dd17cab16445de1
Filmsuphanut <nongfilm2987@gmail.com>
2021-02-13 01:43:02 +0700

    Update Navbar.vue

--- myapp/src/components/Navbar.vue
+++ myapp/src/components/Navbar.vue
@@ -8,16 +8,12 @@
     
       <div class="d-flex align-center">
 
-
-
       </div>
           <v-btn flat herf="#" taget="_blank" >
           <v-icon left>home</v-icon>
           <span>หน้าa</span>
           </v-btn>
       <v-spacer></v-spacer>
-
-
 
 
           <v-btn flat herf="#" taget="_blank" >
